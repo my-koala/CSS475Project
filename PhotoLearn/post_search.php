503e0d89a123cc95bf081e3863bfde73942e1e03
unknown <mykoaladev@gmail.com>
2025-06-04 01:28:14 -0700

    test

--- PhotoLearn/post_search.php
+++ PhotoLearn/post_search.php
@@ -10,8 +10,31 @@
 // Getting the information for the config file
 require_once 'config.inc.php';
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database, $port);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+class PostComment {
+    public $comment_id = 0;
+    public $comment_user_id = 0;
+    public $comment_username = "";
+    public $comment_like_count = 0;
+}
+
+class Post {
+    public $post_id = 0;
+    public $post_username = "";
+    public $post_title = "";
+    public $post_timestamp = "";
+    public $post_text = "";
+    public $post_like_count = 0;
+    
+}
+
 // Placeholder for actual search logic
-$searchResults = "";
+$searchResult = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $searchTagsString = $_POST['tags'] ?? "";
@@ -23,10 +46,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }, $searchTags);
     
     $sql = "SELECT * FROM Posts";
+    $sql .= " INNER JOIN Users ON Posts.user_id = Users.user_id";
+    $sql .= " INNER JOIN PostPhotos ON PostPhotos.post_id = Posts.post_id";
+    $sql .= ";";
     
-    $searchResults = "Tags included: ";
+    $sql_result = $conn->query($sql);
+    
+    $searchResult = "Tags included: ";
     foreach ($searchTags as $searchTag) {
-        $searchResults .= $searchTag . " | ";
+        $searchResults .= $searchTag . ", ";
     }
 }
 
@@ -48,15 +76,20 @@ require_once 'header.inc.php';
             <input type="text" name="tags" placeholder="Enter tags separated by commas" required>
             <input type="submit" value="Search">
         </form>
-
-        <?php if ($searchResults): ?>
+        
+        <?php while ($row = $sql_result->fetch_assoc()): ?>
         <div class="results">
             <?php
-            echo $searchResults;
+            echo htmlspecialchars($row['post_id']) . "\n";
+            echo htmlspecialchars($row['user_id']);
             ?>
         </div>
-        <?php endif; ?>
+        <?php endwhile; ?>
     </div>
 </body>
 
 </html>
+
+<?php
+$conn->close();
+?>
