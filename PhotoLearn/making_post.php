e24422c8146b553b2a605804302c42c4256b738e
IAMCAT2.0 <gehoumingmad@gmail.com>
2025-06-04 03:30:12 -0700

    as

--- PhotoLearn/making_post.php
+++ PhotoLearn/making_post.php
@@ -10,7 +10,7 @@ $message = "";
 $uploadDir = "uploads/";
 if (!file_exists($uploadDir)) mkdir($uploadDir);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST["POST"])) {
     $user_id = intval($_POST['user_id']);
     $post_text = trim($_POST['post_text']);
     $image_description = trim($_POST['image_description']);
@@ -54,7 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 // Handle form submission
-    if ($_SERVER["REQUEST_METHOD"] == "update") {
+    if (isset($_POST["update"])) {
         $post_id = intval($_POST['post_id']);
         $photo_id = intval($_POST['photo_id']);
 
